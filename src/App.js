57a53c3396e34bbe6cc0dfa1699620a59b66f628
vinibosque122 <157661127+vinibosque122@users.noreply.github.com>
2024-07-12 01:46:23 -0700

    Added files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './Argen.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="mainPage">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className='mainText'>
+        <p>
+          Fede and Vini's 440 Project
+        </p>  
+        </div>      
+      </header>
+    </div>
+  );
+}
+
+export default App;
